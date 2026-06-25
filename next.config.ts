ab37c024fe25631c1d6c611367ce0b359f58477c
imomochiimpact <wingkitty2108@gmail.com>
2026-06-25 23:07:18 +0900

    wa-

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        // 画像のホスト名に合わせて変更してください
+        // 例: "example.com" や "drive.google.com" など
+        hostname: "r2.r-sai2026.site",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
